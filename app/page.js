a3515f9bdac8cd481e494d26ea1c63aad6aa45e3
Wim Stalmans <wim.stalmans@gmail.com>
2024-05-22 09:53:16 +0200

    None

--- app/page.js
+++ app/page.js
@@ -1,20 +1,13 @@
-import { useState } from 'react';
-import './index.css';
+import LikeButton from './like-button';
 
- 
+
 function Header({ title }) {
   return <h1>{title ? title : 'Default title'}</h1>;
 }
  
 export default function HomePage() {
   const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
- 
-  const [likes, setLikes] = useState(0);
- 
-  function handleClick() {
-    setLikes(likes + 1);
-  }
- 
+  
   return (
     <div>
       <Header title="Develop. Preview. Ship." />
@@ -23,8 +16,7 @@ export default function HomePage() {
           <li key={name}>{name}</li>
         ))}
       </ul>
- 
-      <button onClick={handleClick}>Like ({likes})</button>
-    </div>
+      <LikeButton />
+     </div>
   );
 }
